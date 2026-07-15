842b378b73c23c36183e5f0677975a1b9312cad6
SUDHIKSHA-CODES <sudhiksha863@gmail.com>
2026-07-15 22:43:33 +0530

    Add predefined chatbot responses

--- codealpha.py
+++ codealpha.py
@@ -4,3 +4,18 @@ print("Welcome to Basic Chatbot!")
 user_input = input("You: ")
 
 print("You entered:", user_input)
+print("Welcome to Basic Chatbot!")
+
+user_input = input("You: ").lower()
+
+if user_input == "hello":
+    print("Bot: Hi!")
+
+elif user_input == "how are you":
+    print("Bot: I'm fine, thanks!")
+
+elif user_input == "bye":
+    print("Bot: Goodbye!")
+
+else:
+    print("Bot: I don't understand.")
